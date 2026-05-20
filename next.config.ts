a8b2edd2a73c9e692c15a443a8a1eaed74a03443
Md Junayet <136969567+mdjunayet10@users.noreply.github.com>
2026-05-20 17:29:46 +0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
